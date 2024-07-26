7ae4146fcfce283c0ae35015da97fed03416cc2b
khuong <khuongavip@gmail.com>
2024-07-26 16:38:38 +0700

    1 6 7

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -1,25 +1,36 @@
 def nil_array(number)
   # return an array containing `nil` the given number of times
+  array = []
+  array.push(nil) * number
 end
 
 def first_element(array)
   # return the first element of the array
+  array[0]
 end
 
 def third_element(array)
   # return the third element of the array
+  array[2]
 end
 
 def last_three_elements(array)
   # return the last 3 elements of the array
+  if array.length > 2
+    array[-3..-1]
+  else
+    array
+  end
 end
 
 def add_element(array)
   # add an element (of any value) to the array
+  array.push("hello world")
 end
 
 def remove_last_element(array)
   # Step 1: remove the last element from the array
+  array.pop
 
   # Step 2: return the array (because Step 1 returns the value of the element removed)
   array
@@ -27,6 +38,9 @@ end
 
 def remove_first_three_elements(array)
   # Step 1: remove the first three elements
+  for _ in 1..3 do
+    array.delete_at(0)
+  end
 
   # Step 2: return the array (because Step 1 returns the values of the elements removed)
   array
@@ -34,28 +48,35 @@ end
 
 def array_concatenation(original, additional)
   # return an array adding the original and additional array together
+  original + additional
 end
 
 def array_difference(original, comparison)
   # return an array of elements from the original array that are not in the comparison array
+  original.difference(comparison)
 end
 
 def empty_array?(array)
+  array.empty?
   # return true if the array is empty
 end
 
 def reverse(array)
+  array.reverse
   # return the reverse of the array
 end
 
 def array_length(array)
+  array.length
   # return the length of the array
 end
 
 def include?(array, value)
+  array.include?(value)
   # return true if the array includes the value
 end
 
 def join(array, separator)
+  array.join(separator)
   # return the result of joining the array with the separator
 end
