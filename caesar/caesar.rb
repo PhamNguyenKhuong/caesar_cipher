e6a1d5618479396b1eec859281b9d7e0d0c4f3a9
khuong <khuongavip@gmail.com>
2024-08-16 18:48:25 +0700

    First try with rubocop

---
+++ caesar/caesar.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+def caesar_cipher(string, num)
+  string.bytes.map do |char|
+    if char.between?(97, 122)
+      (((char + num - 97) % 26) + 97).chr
+    elsif char.between?(65, 90)
+      (((char + num - 65) % 26) + 65).chr
+    else
+      char.chr
+    end
+  end.join
+end
+
+puts caesar_cipher('Hello world', 5)
